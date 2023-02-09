2f240bdc4dd09047dcf2592b5298b5b0e5405100
kundan <goswamikundan123@gmail.com>
2023-02-09 18:38:57 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisionComputation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program.");
+        }
+    }
+}
